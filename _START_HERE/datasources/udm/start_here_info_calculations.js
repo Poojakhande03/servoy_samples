38ecdacc9fff9331a4ab1e57da6eb5c71c6c9c25
Deian Mihutoni <dmihutoni@servoy.com>
2011-10-14 08:34:50 +0000

    Removed bgRowCalculations since solutions use css styling.

--- _START_HERE/datasources/udm/start_here_info_calculations.js
+++ _START_HERE/datasources/udm/start_here_info_calculations.js
@@ -1,19 +1,4 @@
 /**
- * // TODO generated, please specify type and doc for the params
- * @param {Number} index
- * @param {Boolean} selected
- *
- * @properties={typeid:36,uuid:"945F8DE4-CDA4-4350-9756-E0BE15CC8FEA"}
- */
-function bgRowColor_ltGrey(index, selected) {
-	if (selected){
-	   return globals.core_color_bgRowColor; // light grey for the selected row
-	}else{ 
-		return globals.core_color_defaultBgColor; //white
-	}
-}
-
-/**
  * @properties={type:12,typeid:36,uuid:"e6e416f1-c5d3-4a91-b8f2-ca1e55fbe7f8"}
  */
 function icon_display() {
