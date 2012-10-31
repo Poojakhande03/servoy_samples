89196709b41c074fdee2d338129128730b2f50c1
Laurian Vostinar <lvostinar@servoy.com>
2012-10-31 11:39:03 +0000

    implement put (edit)

---
+++ servoy_sample_mobile_service/forms/data_contacts.js
@@ -0,0 +1,17 @@
+/**
+ * @properties={typeid:24,uuid:"0449FDA9-4E04-4EB8-AC4A-47B8B75C988B"}
+ * @AllowToRunInFind
+ */
+function ws_update(data,version,pk)
+{
+	if (foundset.find())
+	{
+		foundset.contact_id = pk;
+		foundset.search();
+		foundset.name_first = data.name_first;
+		foundset.name_last = data.name_last;
+		foundset.email = data.email;
+		foundset.phone_cell = data.phone_cell;
+		foundset.loadAllRecords();
+	}
+}
