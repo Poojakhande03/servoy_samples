62b797368cf7d5237aaad041c6f3c36d5c8ddbd7
Rob Gansevles <rgansevles@servoy.com>
2013-01-09 20:30:41 +0000

    Change underlying persist model for inset lists Case: SVY-3618 [Mobile] Self relation usage in mobile sample solutions Case: SVY-3619

--- servoy_sample_mobile/forms/companies.js
+++ servoy_sample_mobile/forms/companies.js
@@ -24,3 +24,19 @@ function onSyncAction(event)
 function onShow(firstShow, event) {
 	if (foundset.loadRecords) foundset.loadRecords(39)
 }
+
+
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"DFB966A7-FA63-430E-94DC-7C5E2A646210"}
+ */
+function onDetailAction(event) 
+{
+	forms.contacts.showContacts(accountmanager_to_companies.companies_to_contacts);
+}
+
--- servoy_sample_mobile/forms/contacts.js
+++ servoy_sample_mobile/forms/contacts.js
@@ -44,3 +44,18 @@ function onNewAction(event)
 	foundset.newRecord();
 	forms.contact_edit.showContact(foundset);
 }
+
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"90268929-8B65-4991-99CF-BCA34FFB647B"}
+ */
+function onDetailsAction(event) 
+{
+	forms.contact.showContact(foundset);
+}
+
