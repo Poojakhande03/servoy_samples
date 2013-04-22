1735801a827690feb71f308397f1e72bb36a4d97
Ovidiu Buligan <obuligan@servoy.com>
2013-04-22 13:49:01 +0000

    arguments is irrelevant for the given select query replace with null

--- servoy_sample_mobile_service_importhook/globals.js
+++ servoy_sample_mobile_service_importhook/globals.js
@@ -6,7 +6,7 @@
  */
 function onSolutionOpen() 
 {
-	var ds = databaseManager.getDataSetByQuery("udm", "select * from companies where accountmanager_id is not null", arguments, 100);
+	var ds = databaseManager.getDataSetByQuery("udm", "select * from companies where accountmanager_id is not null", null, 100);
 	if (ds.getMaxRowIndex() <= 0)
 	{
 		plugins.rawSQL.executeSQL("udm", "companies", "update companies set accountmanager_id = 39");
